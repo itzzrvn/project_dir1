ad9ab306b4b7218206298e8bba2eccad2cec19e2
itzzrvn <ravnnoledann@gmail.com>
2025-11-14 15:33:46 +0800

    Added code to idkk.c

---
+++ idkk.c
@@ -0,0 +1,20 @@
+//OLEDAN, RAVEN LANCE M. DIT 1-2
+#include <stdio.h>     
+
+int main () {
+
+    int a = 18;
+    float b = 5.8;
+    char c = 'M' ;
+    char d [] = "Raven";
+    double e = 95.1256;
+
+    printf ("Age: %d\n", a);
+    printf ("Height: %.1f\n", b);
+    printf ("Initial: %c\n", c);
+    printf ("Name: %s\n", d);
+    printf ("Average: %.2lf\n", e);
+    printf ("sample changes");
+    return 0;
+        
+}
